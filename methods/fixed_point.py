c7991cc674e13f1b1f0b6ae4d77dae91cdebf1ba
NJL <58279268+njlgbrl@users.noreply.github.com>
2021-04-01 11:48:29 +0800

    Used tabulate module for output

--- methods/fixed_point.py
+++ methods/fixed_point.py
@@ -1,3 +1,9 @@
+from tabulate import tabulate
+
+headers = ['[I]']
+floatformat = [None]
+
+
 class Formula:
     def __init__(self, formula: str, n: float):
         self.formula = formula
@@ -9,7 +15,20 @@ class Formula:
 
 
 def fixed_point(formulae: list) -> list:
-    pass
+    data = []
+    count = 0
+    condition = True
+
+    while condition:
+        row = [count]
+        for i in range(len(formulae)):
+            row.append(formulae[i].curr_Xn)
+            row.append(100.0)
+        data.append(row)
+        count += 1
+        condition = False
+
+    print(tabulate(data, headers=headers, floatfmt=floatformat))
 
 
 if __name__ == '__main__':
@@ -20,6 +39,6 @@ if __name__ == '__main__':
     for i in range(formula_range):
         f = input(f'{chr(ord("f")+i)}(x) >> ')
         formulae.append(Formula(formula=f, n=n))
-
-    for i in range(formula_range):
-        print(f'{chr(ord("f")+i)}(x) = {formulae[i].formula}, Xn = {formulae[i].curr_Xn}')
+        headers.extend([f'{chr(ord("f")+i)}(x)', f'{chr(ord("f")+i)}(x) %e'])
+        floatformat.extend(['.4f', '.4f'])
+    fixed_point(formulae=formulae)

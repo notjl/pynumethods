bc378c3dced6a6aa8b743e080c9ad5c5f7115086
NJL <58279268+notjl@users.noreply.github.com>
2021-04-05 07:32:28 +0800

    bisection function returns root and dictionary
    Commented out how to iterate or tabulate the dictionary.
    
    Addendum:
    Dictionary contains keys that'll help user what the value is. The pri-
    mary key is the iteration count, to guide the user what iteration that
    nested dictionary is.

--- methods/refactor_bisection.py
+++ methods/refactor_bisection.py
@@ -14,6 +14,7 @@ transformations = standard_transformations + \
 
 
 def bisection(f: Function, a: float, b: float, error: float) -> Tuple[float, dict]:
+    data = {}
     count = 1
 
     while True:
@@ -22,6 +23,9 @@ def bisection(f: Function, a: float, b: float, error: float) -> Tuple[float, dic
         fc = f.subs(x, c)
         bc = b-c
 
+        data[count] = {'a': a, 'b': b, 'c': c, 'fa': fa, 'fc': fc, 'bc': bc,
+                       'swap': 'b = c' if fa * fc <= 0 else 'a = c'}
+
         if bc <= error or round(bc, 4) <= error:
             break
 
@@ -29,10 +33,10 @@ def bisection(f: Function, a: float, b: float, error: float) -> Tuple[float, dic
             b = c
         else:
             a = c
-        
-        count +=1
 
-    return c
+        count += 1
+
+    return c, data
 
 
 def main():
@@ -41,7 +45,19 @@ def main():
     b = float(sympify(input('b Value >> ')))
     epsilon = float(sympify(input('Error Tolerance >> ')))
 
-    root = bisection(f=formula, a=a, b=b, error=epsilon)
+    # root, data = bisection(f=formula, a=a, b=b, error=epsilon)
+    # data_to_list: list = []
+    # for data_key, data_value in data.items():
+    #     row: list = [data_key]
+    #     temp: list = []
+    #     for value_data_key, value_data_value in data_value.items():
+    #         temp.append(value_data_value)
+    #     row.extend(temp)
+    #     data_to_list.append(row)
+
+    # headers = ['[I]', 'a', 'b', 'c', 'f(a)', 'f(c)', '(b-c)', 'swap']
+    # floatformat = (None, '.4f', '.4f', '.4f', '.4f', '.4f', '.4f', None)
+    # print(tabulate(data_to_list, headers=headers, floatfmt=floatformat))
 
 
 if __name__ == '__main__':

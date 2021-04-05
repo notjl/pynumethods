3bf985df3f706523c12787e6bc2be60a74960d9f
NJL <58279268+notjl@users.noreply.github.com>
2021-04-05 16:17:33 +0800

    Readying for packaging

--- methods/bracketing/__init__.py
+++ methods/bracketing/__init__.py
@@ -6,7 +6,7 @@ from sympy.parsing.sympy_parser import (convert_xor, function_exponentiation,
                                         implicit_multiplication_application,
                                         parse_expr, standard_transformations)
 
-from .error_handler import SameSigns
+from ..error_handler import SameSigns
 
 x = Symbol('x')
 transformations = standard_transformations + \
@@ -71,7 +71,7 @@ def bisection(f: Function, a: float, b: float, error: float,
 
     fc :
         - Value of the f(c)
-    
+
     bc :
         - Value of (b - c)
 
@@ -135,6 +135,134 @@ def bisection(f: Function, a: float, b: float, error: float,
         if bc <= error or round(bc, 4) <= error:
             break
 
+        if fa*fc <= 0:
+            b = c
+        else:
+            a = c
+
+        count += 1
+
+    if not rational:
+        c = float(c)
+
+    if iterated_data:
+        return c, data
+    else:
+        return c
+
+
+def false_position(f: Function, a: float, b: float, rational: bool = False,
+                   iterated_data: bool = False) -> Union[float, Tuple[float, dict]]:
+    """
+    Using false-position method, returns root (or tupled with iterated data)
+    - The same as bisection method, because the size of an interval
+    containing a root is reduced through iteration.
+
+    ..Note:
+        - Return iterated_data defaults to False
+        - Always use 'x' as symbol
+
+    Algorithm
+    =========
+    1. Find two numbers (a, b) of which f(x) has different signs
+    2. Define c = (a*fb-b*fa)/(fb-fa)
+    3. if b and c are converging, return c as root
+    4. if f(a)f(c) <= 0, b = c else a = c
+
+    Examples
+    ========
+    ** Non-pythonic expressions will be parsed by SymPy module
+    >>> false_position('x^2-8x+11', 1, 2)
+    1.7639484978540771
+
+    ** Using pythonic expressions is also accepted
+    >>> false_position('x**2-8*x+11', 1, 2)
+    1.7639484978540771
+
+    ** Turning rational True
+    >>> false_position('x^3-3x+1', 0, 1, rational=True)
+    119624146949151814554649/344443180703677909347131
+
+    ** Turning iterated_data True
+    >>> false_position('x^3-3x+1', 0, 1, iterated_data=True)
+    (0.3472971847047936, {1: {'a': 0, 'b': 1, 'fa': 1, 'fb': -1, 'c': 1/2, 'fc': -3/8, 'CoS': '-'}, ...)
+
+    Iterated data
+    =============
+    count / iteration :
+        - The first key you see in the dictionary
+
+    a :
+        - Value of the first interval
+
+    b :
+        - Value of the second interval
+
+    fa :
+        - Value of the f(a)
+
+    fb :
+        - Value of the f(b)
+
+    c :
+        - Value of (a*fb-b*fa)/(fb-fa) [value of the root]
+
+    fc :
+        - Value of the f(c)
+
+    Exceptions
+    ==========
+    SameSign :
+        - Raised when values of f(a) and f(b) has the same sign
+
+    Parameters
+    ==========
+    f :
+        - Should be STRING
+        - Mathematical expression
+        - A mathematical function
+        - Example: 
+            'x^2-8x+11'
+            'x**3-3*x+1'
+            'x^3+10x^2-5'
+
+    a :
+        - The first interval
+
+    b :
+        - The second interval
+
+    rational :
+        - Returns fraction/rational value
+        - Defaults to False
+
+    iterated_data:
+        - Returns the iterated data in dictionary
+        - Defaults to False
+    """
+    from math import isclose
+
+    f = parse_expr(f, transformations=transformations)
+    a = sympify(a)
+    b = sympify(b)
+
+    if f.subs(x, a) * f.subs(x, b) > 0.0:
+        raise SameSigns
+
+    data = {}
+    count = 1
+
+    while True:
+        fa = f.subs(x, a)
+        fb = f.subs(x, b)
+        c = (a*fb-b*fa)/(fb-fa)
+        fc = f.subs(x, c)
+
+        data[count] = {'a': a, 'b': b, 'fa': fa, 'fb': fb, 'c': c,
+                       'fc': fc}
+
+        if isclose(b, c, rel_tol=1e-4):
+            break
         if fa*fc <= 0:
             b = c
         else:
--- methods/non_bracketing/__init__.py
+++ methods/non_bracketing/__init__.py
@@ -7,11 +7,128 @@ from sympy.parsing.sympy_parser import (convert_xor, function_exponentiation,
                                         implicit_multiplication_application,
                                         parse_expr, standard_transformations)
 
-from .error_handler import InfiniteIteration
+from ..error_handler import InfiniteIteration
 
 x = Symbol('x')
 transformations = standard_transformations + \
     (convert_xor, implicit_multiplication_application, function_exponentiation)
+
+
+def fixed_point(f: Function, n: float, rational: bool = False,
+                iterated_data: bool = False) -> Union[float, Tuple[float, list]]:
+    """
+    Using Fixed-Point method, returns float (or tupled with iterated data).
+    - Known as the method of successive submission.
+
+    ..Note:
+        - Return iterated_data defaults to False
+        - Always use 'x' as a symbol
+        - Originally was to take list of formulae. Modularising thos method
+        means the need to opt it out.
+
+    ..Usage:
+        * Find all the possible iterative formula of f(x),
+        Transform them to x = f(x)
+            Example:
+                f(x) = x^2-8x+11 can be transformed to:
+                    x = (x^2+11)/8;
+                    x = sqrt(8x-11)
+                    x = (8x-11)/x
+
+    Algorithm
+    =========
+    1. Solve for the new value of n [f(n)] 
+    2. If error % is converging to 0, return n as root
+    3. if previous n is converging with current n, return n as root
+
+    Examples
+    ========
+    ** Non-pythonic expressions will be parsed by SymPy module
+    >>> fixed_point('(x^2+11)/8', 3)
+    1.7639910281905442
+
+    ** Using pythonic expressions is also accepted
+    >>> fixed_point('(x**2+11)/8', 3)
+    1.7639910281905442
+
+    ** Turning rational True
+    >>> fixed_point('(8x-11)/x', 3, rational=True)
+    153301943/24583261
+
+    ** Turning iterated_data True
+    >>> fixed_point('(8x-11)/x', 3, iterated_data=True)
+    (6.236029589402317, {0: {'Xn': 3, 'e%': None}, ...)
+
+    Iterated data
+    =============
+    count / iteration :
+        - The first key you see in the dictionary
+
+    Xn :
+        - Value of the iterated approximate root
+
+    e% :
+        - Percent error, how accurate the approximate root
+
+    Exceptions
+    ==========
+    InfiniteIteration :
+        - Raised when the function passed is indefinite and
+        iteration reaches 99, assuming it is indefinite.
+
+    Parameters
+    ==========
+    f :
+        - Should be STRING
+        - Mathematical expression
+        - A mathematical function
+        - Example: 
+            '(x^2+11)/8'
+            '(x**2+11)/8'
+            '(8x-11)/x'
+
+    n :
+        - the 'x' value of the function
+
+    rational :
+        - Returns fraction/rational value
+        - Defaults to False
+
+    iterated_data:
+        - Returns the iterated data in dictionary
+        - Defaults to False
+    """
+
+    f = parse_expr(f, transformations=transformations)
+    n = sympify(n)
+
+    data = {}
+    count = 0
+    prev_n = 0
+
+    while True:
+        error = abs(((n-prev_n)/n)*100)
+        data[count] = {'Xn': n, 'e%': error if count > 0 else None}
+
+        prev_n = n
+        n = f.subs(x, n)
+
+        if isclose(error, 0.0000, rel_tol=1e-4):
+            break
+        elif isclose(prev_n, n, abs_tol=1e-4):
+            break
+        elif count >= 99:
+            raise InfiniteIteration
+
+        count += 1
+
+    if not rational:
+        n = float(n)
+
+    if iterated_data:
+        return n, data
+    else:
+        return n
 
 
 def newton_raphson(f: Function, n: float, rational: bool = False,
@@ -64,7 +181,7 @@ def newton_raphson(f: Function, n: float, rational: bool = False,
 
     fpx :
         - Value of the f'(n) [computed derived f(n)]
-    
+
     e% :
         - Percent error, how accurate the approximate root
 

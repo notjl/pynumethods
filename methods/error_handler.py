9538a40f8161146c601c6f477be7a5c08603c1eb
NJL <58279268+notjl@users.noreply.github.com>
2021-04-05 11:03:16 +0800

    Fixed error_handling

--- methods/error_handler.py
+++ methods/error_handler.py
@@ -1,8 +1,3 @@
-class Error(Exception):
-    '''Base class for custom errors'''
-    pass
-
-
 class SameSigns(Exception):
     '''Raised when values of f(a) and f(b) has the same sign'''
     def __init__(self, message='f(a) and f(b) values has the same sign') -> None:
@@ -10,12 +5,8 @@ class SameSigns(Exception):
         super().__init__(self.message)
 
 
-class InfiniteIteration(Error):
+class InfiniteIteration(Exception):
     '''Raised when the iteration reaches 99 or greater'''
-    pass
-
-def error_printer(mode='default', count=99):
-    if mode == 'default':
-        print(f'\n\nSince I[{count}] >= 99, it is assumed it as an indefinite function.')
-        print('Resulting in no definite or approximite root.')
-        print('Re-check the function if it is correct.\n')
+    def __init__(self, message='Iteration reached 99, it is assumed the passed function is indefinite') -> None:
+        self.message = message
+        super().__init__(self.message)
